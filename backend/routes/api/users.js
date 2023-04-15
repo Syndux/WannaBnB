05598fa35b4a1e44bb7ddb03ce8a2d657b8cfe83
Huey Nguyen <syndrumablez@gmail.com>
2023-04-15 00:04:25 -0700

    Auth

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -8,40 +8,4 @@ const { User } = require("../../db/models");
 
 const router = express.Router();
 
-// Log In a User
-router.post("/login", async (req, res, next) => {
-  const { credential, password } = req.body;
-
-  const user = await User.unscoped().findOne({
-    where: {
-      [Op.or]: {
-        username: credential,
-        email: credential,
-      },
-    },
-  });
-
-  console.log(user);
-  
-  if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-    const err = new Error("Invalid credentials");
-    err.status = 401;
-    err.title = "Invalid credentials";
-    err.errors = { credential: "Invalid credentials" };
-    return next(err);
-  }
-
-  const safeUser = {
-    id: user.id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    email: user.email,
-    username: user.username,
-  };
-
-  await setTokenCookie(res, safeUser);
-
-  return res.json({ user: safeUser });
-});
-
 module.exports = router;
