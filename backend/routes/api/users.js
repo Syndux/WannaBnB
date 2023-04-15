998e57b7106c6ddd0f3c7bae281d86422b9b72de
Huey Nguyen <syndrumablez@gmail.com>
2023-04-15 15:25:18 -0700

    Signup and Login validation fixes

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -13,28 +13,29 @@ const router = express.Router();
 const validateLogin = [
   check("credential")
     .exists({ checkFalsy: true })
-    .notEmpty()
-    .withMessage("Please provide a valid email or username."),
+    .withMessage("Email or username is required"),
   check("password")
     .exists({ checkFalsy: true })
-    .withMessage("Please provide a password."),
+    .withMessage("Password is required"),
   handleValidationErrors,
 ];
 
 const validateSignup = [
   check("email")
     .exists({ checkFalsy: true })
+    .withMessage("Email is required"),
+  check("email")
     .isEmail()
-    .withMessage("Please provide a valid email."),
+    .withMessage("Invalid email"),
   check("username")
     .exists({ checkFalsy: true })
-    .isLength({ min: 4 })
-    .withMessage("Please provide a username with at least 4 characters."),
-  check("username").not().isEmail().withMessage("Username cannot be an email."),
-  check("password")
+    .withMessage("Username is required"),
+  check("firstName")
     .exists({ checkFalsy: true })
-    .isLength({ min: 6 })
-    .withMessage("Password must be 6 characters or more."),
+    .withMessage("First Name is required"),
+  check("lastName")
+    .exists({ checkFalsy: true })
+    .withMessage("Last Name is required"),
   handleValidationErrors,
 ];
 
