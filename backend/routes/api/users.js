26d344e92cdd123a01cd1bb3652c8fe806424d03
Huey Nguyen <syndrumablez@gmail.com>
2023-04-15 11:18:49 -0700

    Added signup route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -8,4 +8,29 @@ const { User } = require("../../db/models");
 
 const router = express.Router();
 
+// Signup
+router.post('', async (req, res) => {
+  const { firstName, lastName, email, username, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password);
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    username,
+    hashedPassword,
+  });
+
+  const safeUser = {
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    username: user.username,
+  };
+
+  safeUser.token = await setTokenCookie(res, safeUser);
+
+  return res.json({ user: safeUser });
+});
+
 module.exports = router;
