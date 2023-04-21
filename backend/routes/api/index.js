75ee093136ed04404794f7baea2390818a51b665
Huey Nguyen <syndrumablez@gmail.com>
2023-04-20 19:32:27 -0700

    spots update

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,7 @@
 // backend/routes/api/index.js
 const router = require("express").Router();
 const usersRouter = require("./users.js");
+const spotsRouter = require("./spots.js");
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -9,6 +10,7 @@ const { restoreUser } = require("../../utils/auth.js");
 router.use(restoreUser);
 
 router.use("/users", usersRouter);
+router.use("/spots", spotsRouter);
 
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
