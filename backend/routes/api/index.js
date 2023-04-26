7938c15bb78b93d2d6a578ad485d3c4989046fa7
Huey Nguyen <syndrumablez@gmail.com>
2023-04-26 13:26:21 -0700

    add bookings router to api router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const usersRouter = require("./users.js");
 const spotsRouter = require("./spots.js");
 const reviewsRouter = require("./reviews.js");
+const bookingsRouter = require("./bookings.js");
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -13,6 +14,7 @@ router.use(restoreUser);
 router.use("/users", usersRouter);
 router.use("/spots", spotsRouter);
 router.use("/reviews", reviewsRouter);
+router.use("/bookings", bookingsRouter);
 
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
