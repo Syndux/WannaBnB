6a98e4f061542a759369a4e4595039718f6b9f91
Huey Nguyen <syndrumablez@gmail.com>
2023-04-26 14:56:16 -0700

    get current bookings

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -9,4 +9,42 @@ const { User, Booking, Spot, Review, Image, sequelize } = require("../../db/mode
 
 const router = express.Router();
 
+router.get("/current", requireAuth, async (req, res, next) => {
+  const userId = req.user.id;
+
+  const bookings = await Booking.findAll({
+    where: { userId },
+    include: {
+      model: Spot,
+      attributes: {
+        exclude: ["description", "createdAt", "updatedAt"],
+      },
+      include: {
+        model: Image,
+        where: { preview: true },
+        attributes: [],
+      },
+    },
+  });
+
+  for (const booking of bookings) {
+    booking.dataValues.startDate = booking.startDate.toISOString().substring(0, 10);
+    booking.dataValues.endDate = booking.endDate.toISOString().substring(0, 10);
+
+    booking.dataValues.createdAt = booking.createdAt.toISOString().replace("T", " ").substring(0, 19);
+    booking.dataValues.updatedAt = booking.updatedAt.toISOString().replace("T", " ").substring(0, 19);
+
+    const spotPreviewImage = await Image.findOne({
+      where: { imageableId: booking.Spot.id, imageableType: "Spot", preview: true },
+      attributes: ["url"],
+    });
+
+    booking.Spot.dataValues.previewImage = spotPreviewImage
+      ? spotPreviewImage.url
+      : null;
+  }
+
+  return res.json({ Bookings: bookings });
+});
+
 module.exports = router;
