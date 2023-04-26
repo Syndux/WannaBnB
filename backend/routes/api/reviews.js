b2948fb7c6cf5b05a9cadd55e27b4d7823a0358d
Huey Nguyen <syndrumablez@gmail.com>
2023-04-26 13:22:19 -0700

    review import refactor

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,9 +1,6 @@
 const express = require("express");
-const { Op } = require("sequelize");
-const bcrypt = require("bcryptjs");
-const { check } = require("express-validator");
 
-const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth");
+const { requireAuth } = require("../../utils/auth");
 const { validateReviewBody } = require("../../utils/validation");
 const { User, Booking, Spot, Review, Image, sequelize } = require("../../db/models");
 
