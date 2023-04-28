fca226a665f4524e42ef21a07e17576187c80b28
Huey Nguyen <syndrumablez@gmail.com>
2023-04-28 13:25:20 -0700

    formatting

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const { Op } = require("sequelize");
-const bcrypt = require("bcryptjs");
-const { check } = require("express-validator");
 
 const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth");
 const { validateBookingBody } = require("../../utils/validation");
 const { User, Booking, Spot, Review, Image, sequelize } = require("../../db/models");
-const { route } = require("./reviews");
 
 const router = express.Router();
 
