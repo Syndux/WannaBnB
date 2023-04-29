082c44e15167f518d4bc50409895040be774852b
Huey Nguyen <syndrumablez@gmail.com>
2023-04-28 23:41:53 -0700

    Delete success

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -6,8 +6,41 @@ const { check } = require("express-validator");
 const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth");
 const { validateBookingBody } = require("../../utils/validation");
 const { User, Booking, Spot, Review, Image, sequelize } = require("../../db/models");
-const { route } = require("./reviews");
 
 const router = express.Router();
+
+// Delete spot image
+router.delete("/:id", requireAuth, async (req, res, next) => {
+  const { id } = req.params;
+  const userId = +req.user.id;
+  
+  const image = await Image.findByPk(id, {
+    include: {
+      model: Spot,
+      attributes: ["ownerId"],
+    }
+  });
+
+  if(!image) {
+    return next({
+      status: 404,
+      message: "Spot Image couldn't be found",
+    });
+  }
+
+  if(userId !== image.Spot.ownerId) {
+    const err = new Error("Authorization required");
+    err.status = 403;
+    err.message = "Forbidden";
+    return next(err);
+  }
+
+  await image.destroy();
+
+  return res.json({
+    status: 200,
+    message: "Successfully deleted",
+  });
+});
 
 module.exports = router;
