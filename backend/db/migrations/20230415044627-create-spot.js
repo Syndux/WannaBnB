4a3f918c4ab4425cfddd73f6c0aee09b01442940
Huey Nguyen <syndrumablez@gmail.com>
2023-04-29 14:31:21 -0700

    migration fix

--- backend/db/migrations/20230415044627-create-spot.js
+++ backend/db/migrations/20230415044627-create-spot.js
@@ -40,11 +40,11 @@ module.exports = {
           allowNull: false,
         },
         lat: {
-          type: Sequelize.DECIMAL(2, 7),
+          type: Sequelize.DECIMAL(2, 9),
           allowNull: false,
         },
         lng: {
-          type: Sequelize.DECIMAL(2, 7),
+          type: Sequelize.DECIMAL(2, 9),
           allowNull: false,
         },
         name: {
