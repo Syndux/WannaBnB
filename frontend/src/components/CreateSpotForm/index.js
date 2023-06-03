a21ecedebf46d3bc8f71113169953b89e4fad523
Huey Nguyen <syndrumablez@gmail.com>
2023-06-03 02:12:58 -0700

    form section 1

--- frontend/src/components/CreateSpotForm/index.js
+++ frontend/src/components/CreateSpotForm/index.js
@@ -1,11 +1,45 @@
 import React from "react";
+import "./CreateSpotForm.css"; // Import the CSS file
 
 function CreateSpotForm() {
   return (
-    <>
-      Create a new spot form here
-    </>
-  )
+    <div className="spot-create-outer-container">
+      <div className="spot-create-inner-container">
+        <h1 className="form-heading">Create a New Spot</h1>
+
+        <div className="spot-create-section-header">
+          <p className="subheading">Where's your place located?</p>
+          <p className="description">
+            Guests will only get your exact address once they booked a reservation.
+          </p>
+        </div>
+
+        <div className="spot-create-section">
+          <form>
+            <label htmlFor="country">Country:</label>
+            <input type="text" id="country" />
+
+            <label htmlFor="street">Street Address:</label>
+            <input type="text" id="street" />
+
+            <label htmlFor="city">City:</label>
+            <input type="text" id="city" />
+
+            <label htmlFor="state">State:</label>
+            <input type="text" id="state" />
+
+            <label htmlFor="latitude">Latitude:</label>
+            <input type="text" id="latitude" />
+
+            <label htmlFor="longitude">Longitude:</label>
+            <input type="text" id="longitude" />
+
+            <button type="submit">Create Spot</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateSpotForm;
