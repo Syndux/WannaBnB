98d528068788870ccd4bdbb07c2d35bd71623352
Huey Nguyen <syndrumablez@gmail.com>
2023-06-07 20:52:41 -0700

    started details page

--- frontend/src/components/LandingPage/SpotImages/index.js
+++
@@ -1,9 +0,0 @@
-export { default as spotImg1Prev } from './spotImg1Prev.jpg';
-export { default as spotImg2Prev } from './spotImg2Prev.jpeg';
-export { default as spotImg3Prev } from './spotImg3Prev.jpeg';
-export { default as spotImg4Prev } from './spotImg4Prev.jpeg';
-export { default as spotImg5Prev } from './spotImg5Prev.jpeg';
-export { default as spotImg6Prev } from './spotImg6Prev.jpeg';
-export { default as spotImg7Prev } from './spotImg7Prev.jpeg';
-
-
--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -1,9 +1,42 @@
+import React from 'react';
+
 const SpotDetails = ({ spot }) => {
+  console.log(spot);
+  const { name, city, state, country, previewImage, images, firstName, lastName, description, price } = spot;
+
+  const reserveSpot = () => {
+    alert('Feature coming soon');
+  };
+
   return (
-    <>
-      Spot Details
-    </>
-  )
-}
+    <div className="spot-details-container">
+      <div className="spot-name">{name}</div>
+      <div className="spot-location">{`${city}, ${state}, ${country}`}</div>
+      <div className="spot-photos">
+        <div className="preview-image">
+          <img src={previewImage} alt="Spot Preview" />
+        </div>
+        <div className="additional-images">
+          {images.map((image, index) => (
+            <div className="additional-image" key={index}>
+              <img src={image} alt={`Spot Image ${index + 1}`} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="hosted-by">
+        Hosted by {firstName} {lastName}
+      </div>
+      <div className="spot-description">{description}</div>
+      <div className="spot-price">
+        <span className="spot-price-value">${price}</span>
+        <span>night</span>
+      </div>
+      <button className="reserve-button" onClick={reserveSpot}>
+        Reserve
+      </button>
+    </div>
+  );
+};
 
 export default SpotDetails;
