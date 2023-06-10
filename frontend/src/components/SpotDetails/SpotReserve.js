6aa3e827e5e7b4ab0464edcc5cfc811ce125fd39
Huey Nguyen <syndrumablez@gmail.com>
2023-06-10 08:55:09 -0700

    spotdetails

--- frontend/src/components/SpotDetails/SpotReserve.js
+++ frontend/src/components/SpotDetails/SpotReserve.js
@@ -1,9 +1,28 @@
-const SpotReserve = ({spot}) => {
+import React from "react";
+
+const SpotReserve = ({ spot }) => {
+  const reserveSpot = () => {
+    alert("Feature coming soon");
+  };
+
   return (
-    <div>
-      
+    <div className="spot-reserve-card">
+      <div className="spot-reserve-info">
+        <div className="spot-price">{`$${spot.price} night`}</div>
+        <div className="spot-avg-rating">
+          <i className="fa-solid fa-star" />
+          {spot.avgStarRating
+            ? spot.avgStarRating.toFixed(
+                Number.isInteger(spot.avgStarRating) ? 1 : 2
+              )
+            : "New"}
+        </div>
+      </div>
+      <button onClick={reserveSpot} className="reserve-button">
+        Reserve
+      </button>
     </div>
-  )
+  );
 };
 
 export default SpotReserve;
