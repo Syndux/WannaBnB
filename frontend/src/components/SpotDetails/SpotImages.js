b95d31d319757ea14e4cf86c4dfcf6b5654c8003
Huey Nguyen <syndrumablez@gmail.com>
2023-06-10 00:42:27 -0700

    spot images

---
+++ frontend/src/components/SpotDetails/SpotImages.js
@@ -0,0 +1,25 @@
+import './SpotImages.css'
+
+const SpotImages = ({ spot }) => {
+  if (!spot.SpotImages) {
+    return null;
+  }
+
+  const previewImage = spot.SpotImages.find((image) => image.preview);
+  const additionalImages = spot.SpotImages.filter((image) => !image.preview);
+
+  return (
+    <div className="spot-images">
+      <div className="preview-image">
+        <img src={previewImage.url} alt="Spot Preview" />
+      </div>
+      {additionalImages.map((image, index) => (
+        <div className="additional-image" key={index}>
+          <img src={image.url} alt={`Spot Image ${index + 1}`} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SpotImages;
--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
-import { getSpotDetails } from '../../store/spots';
+import { getSpotDetails } from "../../store/spots";
+import SpotImages from "./SpotImages";
+import "./SpotDetails.css";
 
 const SpotDetails = () => {
   const { id } = useParams();
@@ -15,39 +17,30 @@ const SpotDetails = () => {
   }, [dispatch, id]);
 
   const reserveSpot = () => {
-    alert('Feature coming soon');
+    alert("Feature coming soon");
   };
 
-  console.log(spot.Owner);
+  return (
+    spot && (
+      <div className="spot-details-container">
+        <div className="spot-name">{spot.name}</div>
+        <div className="spot-location">{`${spot.city}, ${spot.state}, ${spot.country}`}</div>
+        
+        <SpotImages spot={spot} />
 
-  return spot && ( 
-    <div className="spot-details-container">
-      <div className="spot-name">{spot.name}</div>
-      <div className="spot-location">{`${spot.city}, ${spot.state}, ${spot.country}`}</div>
-      <div className="spot-photos">
-        <div className="preview-image">
-          <img src={spot.previewImage} alt="Spot Preview" />
+        {/* <div className="hosted-by">
+          Hosted by {spot.Owner.firstName} {spot.Owner.lastName}
         </div>
-        <div className="additional-images">
-          {spot.SpotImages.map((image, index) => (
-            <div className="additional-image" key={index}>
-              <img src={image} alt={`Spot Image ${index + 1}`} />
-            </div>
-          ))}
+        <div className="spot-description">{spot.description}</div>
+        <div className="spot-price">
+          <span className="spot-price-value">${spot.price}</span>
+          <span>night</span>
         </div>
+        <button className="reserve-button" onClick={reserveSpot}>
+          Reserve
+        </button> */}
       </div>
-      {/* <div className="hosted-by">
-        Hosted by {spot.Owner.firstName} {spot.Owner.lastName}
-      </div>
-      <div className="spot-description">{spot.description}</div>
-      <div className="spot-price">
-        <span className="spot-price-value">${spot.price}</span>
-        <span>night</span>
-      </div>
-      <button className="reserve-button" onClick={reserveSpot}>
-        Reserve
-      </button> */}
-    </div>
+    )
   );
 };
 
