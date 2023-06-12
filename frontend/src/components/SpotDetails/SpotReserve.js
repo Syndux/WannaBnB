72a0bd9a7eb456e8879b830f38d3b6373124d0a0
Huey Nguyen <syndrumablez@gmail.com>
2023-06-12 04:01:11 -0700

    Adjust reserve card styles

--- frontend/src/components/SpotDetails/SpotReserve.js
+++ frontend/src/components/SpotDetails/SpotReserve.js
@@ -8,14 +8,18 @@ const SpotReserve = ({ spot }) => {
   return (
     <div className="spot-reserve-card">
       <div className="spot-reserve-info">
-        <div className="spot-price">{`$${spot.price} night`}</div>
+        <div className="spot-price">
+          {`$${spot.price}`} <span className="spot-price-unit">night</span>
+        </div>
         <div className="spot-avg-rating">
           <i className="fa-solid fa-star" />
-          {spot.avgStarRating
-            ? spot.avgStarRating.toFixed(
-                Number.isInteger(spot.avgStarRating) ? 1 : 2
-              )
-            : "New"}
+          <span className="spot-avg-rating-new">
+            {spot.avgStarRating
+              ? spot.avgStarRating.toFixed(
+                  Number.isInteger(spot.avgStarRating) ? 1 : 2
+                )
+              : "New"}
+          </span>
         </div>
       </div>
       <button onClick={reserveSpot} className="reserve-button">
