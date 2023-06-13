86f22203d1d19764fc06c6bd515860363cf47d58
Huey Nguyen <syndrumablez@gmail.com>
2023-06-12 17:04:17 -0700

    newest review first

--- frontend/src/components/SpotDetails/ReviewsList.js
+++ frontend/src/components/SpotDetails/ReviewsList.js
@@ -1,16 +1,20 @@
-import SingleReview from './SingleReview';
+import React from "react";
+import SingleReview from "./SingleReview";
 
 const ReviewsList = ({ reviews }) => {
-  
+  const sortedReviews = reviews.sort((a, b) => {
+    const dateA = new Date(a.updatedAt);
+    const dateB = new Date(b.updatedAt);
+    return dateB - dateA;
+  });
+
   return (
     <div className="reviews-container">
-      {reviews.map((review) => {
-        return (
-          <SingleReview review={review} key={review.id} />
-        )
-      })}
+      {sortedReviews.map((review) => (
+        <SingleReview review={review} key={review.id} />
+      ))}
     </div>
-  )
+  );
 };
 
 export default ReviewsList;
